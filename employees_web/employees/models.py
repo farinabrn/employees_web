e78c48a0308fe2d91c29e1ecbf08aeb47212dc90
Bruno Farina <bfarina@showcasepro.com.br>
2019-09-02 18:08:51 -0300

    added django-mptt lib

--- employees_web/employees/models.py
+++ employees_web/employees/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 import json
 
 # Create your models here.
 
-class Employee(models.Model):
+class Employee(MPTTModel):
 	STANDARD = 'STD'
 	MANAGER = 'MGR'
 	SR_MANAGER = 'SRMGR'
@@ -22,32 +23,20 @@ class Employee(models.Model):
 	salary = models.IntegerField()
 	cell_phone = models.IntegerField()
 	role = models.CharField(max_length=25, choices=EMPLOYEE_TYPES, default=STANDARD)
-	manager = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='subordinate')
+	parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='subordinate')
 
 	def __str__(self):
 		return self.name
 
-	def get_managed_count(self, id=None):
-		if id is None:
-			id = self.id
-
-		managed_list = Employee.objects.filter(manager=self)
-		managed_count=0
-		for employee in managed_list:
-			managed_count+=employee.get_managed_count(id)
-
-		if self.id != id:
-			managed_count+=1
-
-		return managed_count
+	def get_managed_count(self):
+		return self.get_descendants().count()
 
 	def get_managed_salary(self, id=None):
 		if id is None:
 			id = self.id
 
-		managed_list = Employee.objects.filter(manager=self)
 		managed_salary=0
-		for employee in managed_list:
+		for employee in self.subordinate.all():
 			managed_salary+=employee.get_managed_salary(id)
 
 		if self.id != id:
